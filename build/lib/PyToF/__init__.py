ce95e52097e9ba60f39a19d7018cad875f1712e4
Luca Morf <luca.morf@gmail.com>
2025-01-06 11:11:44 +0100

    Add build/ and PyToF.egg-info/ to .gitignore

--- build/lib/PyToF/__init__.py
+++
@@ -1,6 +0,0 @@
-########################################################
-# Author of this version: Luca Morf - luca.morf@uzh.ch #
-########################################################
-
-from PyToF.ClassToF import ToF
-
