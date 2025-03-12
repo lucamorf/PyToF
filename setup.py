3cd704f677d07c0733001d12d53d044d36d9eb6e
Luca Morf <luca.morf@gmail.com>
2025-03-12 16:15:16 +0100

    major speedup in AlgoToF due to array multiplication avoidance, changes to nmovshov_ToFPlanet to ensure version compatibility, changes to setup.py to specify version number

--- setup.py
+++ setup.py
@@ -13,5 +13,5 @@ setup(
     author_email="luca.morf@uzh.ch",
     license="Mozilla Public License Version 2.0",
     packages=find_packages(include=["PyToF", "PyToF.*"]),
-    install_requires=["numpy", "scipy", "matplotlib", "emcee", "tqdm"],
+    install_requires=["numpy==2.2.3", "scipy==1.15.2", "matplotlib==3.10.1", "emcee==3.1.6", "tqdm==4.67.1"],
 )
